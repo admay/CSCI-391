f6251c45b2d131881577bb891118e9b864857ea6
Michael Zavarella <mzavarella30@gmail.com>
2015-01-22 19:08:48 -0500

    Finished
    - I finished, I think

--- AffineCipher.py
+++ AffineCipher.py
@@ -15,7 +15,7 @@ def encrypt(a, b, mes):
 
 
 def inverse(a):
-    for i in range(26):
+    for i in range(1, 26):
         if (i * a) % 26 == 1:
             return i
 
@@ -31,6 +31,17 @@ def decrypt(a, b, mes):
     print(''.join(output))
 
 
+def brute_force(mes):
+    for a in [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]:
+        for b in range(26):
+            decrypt(a, b, mes)
+            print(a, b)
+            outcome = input("Is this message correct? ")
+            if outcome.lower() in ['y', 'yes']:
+                return 'Shibby'
+
+
+
 def input_function(method):
     user_a = int(input('Please enter the first key, A, that is coprime to 26: '))
     user_b = int(input('Please enter the second key, B, between 0 and 26: '))
@@ -42,11 +53,14 @@ def input_function(method):
         decrypt(user_a, user_b, user_mes)
 
 
-def init_function():
-    x = input("Enter 1 for encrypt or 2 for decrypt: ")
+def init():
+    x = input("Enter 1 for encrypt, 2 for decrypt, or 3 for a brute force decryption: ")
     if x == '1':
         input_function('1')
     elif x == '2':
         input_function('2')
+    elif x == '3':
+        user_mes = input('What would you like to force decrypt: ')
+        brute_force(user_mes)
         
-init_function()
+init()
