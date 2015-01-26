9175cc6856656fc7ec0f3c013329f0a9261a7e2c
Michael Zavarella <mzavarella30@gmail.com>
2015-01-25 21:13:50 -0500

    Comments and shit
    - Finished the program
    - Altered and optimized a little bit of the program
    - Tested the program
    - Commented out the program
    - I am so done with this program that I can't even...

--- AffineCipher.py
+++ AffineCipher.py
@@ -1,17 +1,21 @@
 # Michael Zavarella
 # Project 1 for CSCI 391
-# Affine Cypher... Whatever THAT is
+# This program implements an Affine cypher encryption or decryption
+output = []
 
-# Encrypts the user's message based on their a, b, and message inputs
+
+# Encrypts the user's plaintext message received in the get_input function.
+# The a and b values are taken from the get_key_values function and are used as
+# parameters used in the encryption algorithm. As each individual character is
+# encrypted, it is added to the end of the output array which is then printed as a
+# string of uppercase characters
 def encrypt(a, b, mes):
-    output = []                               
     for l in mes:
         if l.isalpha():             
             c = chr((((a * (ord(l.upper()) - 65)) + b) % 26) + 65)
             output.append(c)            
         else:
             output.append(l)
-    print(''.join(output))
 
 
 # Calculates the multiplicative inverse of a number mod 26
@@ -21,25 +25,29 @@ def inverse(a):
             return i
 
 
-# Decrypts the user's message based on their a, b, and message inputs
+# Decrypts the user's ciphertext that is received from the get_input function.
+# The inverse of a is calculated via the inverse function and used to convert the
+# ciphertext back into plaintext. The output is created in the same way as in the
+# encrypt function.
 def decrypt(a, b, mes):
-    output = []
     for l in mes:
         if l.isalpha():
             p = chr(((inverse(a) * ((ord(l.upper()) - 65) - b)) % 26) + 65)
             output.append(p.lower())
         else:
             output.append(l)
-    print(''.join(output))
 
 
-# Decrypts an encrypted message by trying all possible combinations of a and b
+# Decrypts an encrypted message by trying all possible combinations of a and b values.
+# After every attempt, the a and b values are printed along with the plaintext that was
+# calculated. The user is then asked if the output is correct, if it is then the inputs
+# y or yes will end the program, else hit enter and keep trying.
 def brute_force(mes):
     for a in [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]:
         for b in range(26):
             decrypt(a, b, mes)
             print(a, b)
-            outcome = input("Is this message correct? ")
+            outcome = input("Is this message correct? Press enter to continue decrypting.")
             if outcome.lower() in ['y', 'yes']:
                 return 'Shibby'
 
@@ -50,33 +58,40 @@ def get_key_values():
         user_a = int(input("Enter first key: "))
         user_b = int(input("Enter second key: "))
         if user_a != 13 and user_a % 2 != 0:
-            if 0 < user_b < 26:
+            if 0 <= user_b < 26:
                 check = 0
+            else:
+                print("Invalid B.")
+        else:
+            print("Invalid A.")
     return user_a, user_b
 
 
-# Grabs the user inputs like a boss
-def input_function(method):
+# Grabs the user's inputs for the keys a and b by calling the get_key_values function.
+# Grabs the user's message via a raw input.
+# Takes the method as a parameter to determine whether the user wants to encrypt or decrypt
+# their message. Then, based on the method, it calls either the encrypt or decrypt function.
+def get_input(method):
     user_a, user_b = get_key_values()
+    user_mes = input('Enter your message: ')
     if method == '1':
-        user_mes = input('What would you like to encrypt? ')
         encrypt(user_a, user_b, user_mes)
     elif method == '2':
-        user_mes = input('What would you like to decrypt? ')
         decrypt(user_a, user_b, user_mes)
+    print(''.join(output))
 
 
-# Start up function to get the ball rolling
-def init():
+# Start-up function that begins by asking the user if they want to encrypt, decrypt, or brute
+# force decrypt a message.
+def on_start():
     x = input("Enter 1 for encrypt, 2 for decrypt, or 3 for a brute force decryption: ")
     if x == '1':
-        input_function('1')
+        get_input('1')
     elif x == '2':
-        input_function('2')
+        get_input('2')
     elif x == '3':
         user_mes = input('What would you like to force decrypt: ')
         brute_force(user_mes)
         
 
-# Leggo
-init()
+on_start()
